2b992ab40bbf91b6f979300d97d6d271860d6d42
Voltra <ludwig.guerin.98@gmx.fr>
2020-01-19 12:42:32 +0100

    Update enabledState mixin logic

--- src/mixins/enabledState.js
+++ src/mixins/enabledState.js
@@ -1,19 +1,19 @@
 export default {
     methods: {
         toggleForItem({ name }){
-            return () => this.$gdpr.hasGuard(name) && this.$gdpr.getGuard(name).toggle();
+            return () => this.$gdpr.toggle(name);
         },
         toggleItem(item){
             this.toggleForItem(item)();
         },
         enableForItem({ name }){
-            return () => this.$gdpr.hasGuard(name) && this.$gdpr.getGuard(name).enable();
+            return () => this.$gdpr.enable(name);
         },
         enableItem(item){
             this.enableForItem(item)();
         },
         disableForItem({ name }){
-            return () => this.$gdpr.hasGuard(name) && this.$gdpr.getGuard(name).disable();
+            return () => this.$gdpr.disable(name);
         },
         disableItem(item){
             this.disableForItem(item)()
