ba9d602e2ddf4f4e78896138a17c8f95023d1e07
Voltra <ludwig.guerin.98@gmx.fr>
2020-01-24 09:39:21 +0100

    Expose GdprStorage via the vue plugin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { ManagerWrapperFactory } from "./wrapper"
-import VueDeepMerge from "./plugins/deepmerge"
+// import VueDeepMerge from "./plugins/deepmerge"
 import GdprManager from "./components/GdprManager.vue"
 import GdprGroup from "./components/GdprGroup.vue"
 import GdprGuard from "./components/GdprGuard.vue"
-import { GdprManagerBuilder } from "gdpr-guard"
+import { GdprStorage } from "gdpr-guard"
 
 //TODO: Allow hotswap of managers
 
@@ -13,31 +13,38 @@ const VueGdprGuard = {
      * @type {import("vue/types/plugin").PluginFunction}
      */
     install(Vue, options){
-        Vue.use(VueDeepMerge);
+        // Vue.use(VueDeepMerge);
 
         const ManagerWrapper = ManagerWrapperFactory(Vue);
         const wrapper = new ManagerWrapper(options.manager);
 
-        const desc = {
-            value: wrapper,
+        const desc = val => ({
+            value: val,
             configurable: false,
             enumerable: true,
             writable: false,
-        };
-
-        Object.defineProperties(Vue.prototype, {
-            "$gdpr": desc,
         });
 
-        Object.defineProperties(Vue, {
-            "gdpr": desc,
+        [{
+            key: "gdpr",
+            value: desc(wrapper),
+        }, {
+            key: "GdprStorage",
+            value: desc(GdprStorage),
+        }].forEach(({key, value}) => {
+            Object.defineProperties(Vue.prototype, {
+                [`$${key}`]: value,
+            });
+
+            Object.defineProperties(Vue, {
+                [key]: value,
+            });
         });
     }
 };
 
 export {
     VueGdprGuard,
-    GdprManagerBuilder,
 
     GdprManager,
     GdprGroup,
