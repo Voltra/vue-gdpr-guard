f26d0c53529550df8506bc8f59f5938ba69fc79d
Voltra <ludwig.guerin.98@gmx.fr>
2021-10-24 12:57:21 +0200

    Update deps and extract configs

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+
+module.exports = {
+	"root": true,
+	"env": {
+		"node": true
+	},
+	"extends": [
+		"plugin:vue/essential",
+		"eslint:recommended"
+	],
+	"rules": {},
+	"parserOptions": {
+		"parser": "babel-eslint"
+	}
+};
