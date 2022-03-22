51d4092192ad65802bb2db2b499901c941c8765b
Voltra <ludwig.guerin.98@gmx.fr>
2022-03-22 22:22:25 +0100

    Update readme to new slot syntax

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+
 module.exports = {
 	lintOnSave: true,
 	parallel: true,
