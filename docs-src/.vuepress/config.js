62d5d612756a2cafde2e921685ac1d652cba318a
Voltra <ludwig.guerin.98@gmx.fr>
2022-04-17 17:17:42 +0200

    Fix vuepress config

--- docs-src/.vuepress/config.js
+++ docs-src/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: "/vue-gdpr-guard/",
-    dest: "../docs",
+    dest: "./docs",
     themeConfig: {
         logo: "https://raw.githubusercontent.com/Voltra/vue-gdpr-guard/dev/vue-gdpr-guard.png",
         smoothScroll: true,
